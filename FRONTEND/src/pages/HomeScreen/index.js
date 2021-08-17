52fa31ae54670751c5acb06bcd5b012e40c2d229
Kaminishikawahara KK <kkaori146@gmail.com>
2021-08-18 07:38:06 +0900

    API: Structuring and use

--- FRONTEND/src/pages/HomeScreen/index.js
+++ FRONTEND/src/pages/HomeScreen/index.js
@@ -1,12 +1,22 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { useHistory } from "react-router-dom";
 import { Container } from './styled';
 
 import Header from '../../components/Header';
 
+import api from '../../api';
+
 export default () => {
     const history = useHistory();
     const [headerSearch, setHeaderSearch] = useState('');
+
+    useEffetc(()=>{
+        const getCategories = async () => {
+        const categories = await api.getCategories();
+        console.log(categories);
+        };
+        getCategories();
+    }, []);
 
     return (
         <Container>
