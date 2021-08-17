ef2545d537506da6331a42acc4ed43162634ab73
Kaminishikawahara KK <kkaori146@gmail.com>
2021-08-17 10:51:20 +0900

    Home: Search Area

--- FRONTEND/src/components/Header/index.js
+++ FRONTEND/src/components/Header/index.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Container, Logo, SearchInput} from './styled';
 
 export default () => {
+
+    const [inputActive, setInputActive] = useState(false);
+
+    const handleInputFocus = () => {
+        setInputActive(true);
+    }
+    const handleInputBlur = () => {
+        setInputActive(false);
+    }
+
     return (
         <Container>
             <Logo src="/assets/logo.png" />
 
-            <SearchInput type="text" placeholder="Procurar por..."/>
+            <SearchInput 
+                type="text" 
+                placeholder="Procurar por..."
+                active={inputActive}
+                onFocus={handleInputFocus} //Qdo se coloca o cursor executa esta função
+                onBlur={handleInputBlur} // Qdo tira o cursor executa esta função
+            />
         </Container>
 
     );
